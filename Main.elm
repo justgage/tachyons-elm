d89cb2369d08b170d847c0d15eb35ba4770e34f4
Gage Peterson <justgage@gmail.com>
2017-06-08 20:41:11 -0600

    Polished docs

--- Main.elm
+++ Main.elm
@@ -5,53 +5,7 @@ import Html exposing (..)
 
 
 main =
-    beginnerProgram
-        { model = model
-        , update = update
-        , view = view
-        }
-
-
-
--- TYPES
-
-
-type alias Model =
-    { message : String
-    }
-
-
-type Msg
-    = NoOp
-
-
-
--- MODEL
-
-
-model : Model
-model =
-    { message = "Elm beginnerProgram is ready. Get started!" }
-
-
-
--- UPDATE
-
-
-update : Msg -> Model -> Model
-update msg model =
-    case msg of
-        NoOp ->
-            model
-
-
-
--- VIEW
-
-
-view : Model -> Html Msg
-view model =
     div [ classes [ f1, purple ] ]
         [ tachyons.css
-        , text model.message
+        , text "I'm Purple and big!"
         ]
