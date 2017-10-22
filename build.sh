e122917a4ab87308be9b16e1c9c1702f70e3a2c5
Gage Peterson <justgage@gmail.com>
2017-10-22 08:00:19 -0600

    fix build

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # This will build and make sure things compile
-node scripts/convert.js && elm-format Tachyons/Classes.elm --yes && elm-make Tachyons/Classes.elm && elm-make --docs documentation.json && index.html
+node scripts/convert.js && elm-format Tachyons/Classes.elm --yes && elm-make Tachyons/Classes.elm && elm-make --docs documentation.json
