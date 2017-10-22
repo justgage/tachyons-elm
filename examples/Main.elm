faab3ae1c428bf68c8d2479edd396152151dafe0
Gage Peterson <justgage@gmail.com>
2017-10-22 08:23:28 -0600

    inline css

--- examples/Main.elm
+++ examples/Main.elm
@@ -6,7 +6,7 @@ import Html exposing (..)
 
 
 main =
-    div [ classes [ f1, purple, pointer, b ] ]
+    div [ classes [ f1, purple, pointer, Tachyons.Classes.b ] ]
         [ tachyons.css
         , text "I'm Purple and big!"
         ]
