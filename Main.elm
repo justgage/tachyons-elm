fde344ce606ee7aca77e9d6679d46bd4e9dbd312
Gage Peterson <justgage@gmail.com>
2017-10-24 07:56:06 -0600

    update example

--- Main.elm
+++ Main.elm
@@ -1,7 +1,7 @@
 module Main exposing (..)
 
 import Tachyons exposing (classes, tachyons)
-import Tachyons.Classes exposing (f1, purple, pointer)
+import Tachyons.Classes exposing (f1, purple, pointer, tj)
 import Html exposing (..)
 
 
@@ -10,7 +10,7 @@ This is just an example of how this works
 -}
 main : Html msg
 main =
-    div [ classes [ f1, purple, pointer, Tachyons.Classes.b ] ]
+    div [ classes [ tj, f1, purple, pointer, Tachyons.Classes.b ] ]
         [ tachyons.css
-        , text "I'm Purple and big!"
+        , text "I'm Purple and big! I'm Purple and big! I'm Purple and big! I'm Purple and big! I'm Purple and big! I'm Purple and big! I'm Purple and big! I'm Purple and big! I'm Purple and big! I'm Purple and big! I'm Purple and big! I'm Purple and big! I'm Purple and big! I'm Purple and big! I'm Purple and big! I'm Purple and big! I'm Purple and big! I'm Purple and big! I'm Purple and big! "
         ]
